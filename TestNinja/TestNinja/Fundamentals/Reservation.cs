7c5baa3ca4f6f2537dadae2a3fd44d04fc7dedf2
Warisara33 <warisara.sa@rmuti.ac.th>
2019-10-16 15:08:09 +0700

    1_8TestingAlltheExecution

--- TestNinja/TestNinja/Fundamentals/Reservation.cs
+++ TestNinja/TestNinja/Fundamentals/Reservation.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestNinja.Fundamentals
+﻿namespace TestNinja.Fundamentals
 {
-    class Reservation
+    public class Reservation
     {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+
+            if (MadeBy == user)
+                return true;
+
+            return false;
+        }
+
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
     }
 }
