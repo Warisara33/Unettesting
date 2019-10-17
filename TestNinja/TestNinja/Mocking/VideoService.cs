1486b8672a02a3857a0ade017d3f693b82d74a7f
Warisara33 <warisara.sa@rmuti.ac.th>
2019-10-17 19:27:24 +0700

    6_2 Refactoring

--- TestNinja/TestNinja/Mocking/VideoService.cs
+++ TestNinja/TestNinja/Mocking/VideoService.cs
@@ -8,10 +8,12 @@ namespace TestNinja.Mocking
     public class VideoService
     {
         private IFileReader _fileReader;
+        private IVideoRepository _repository;
 
-        public VideoService (IFileReader fileReader)
+        public VideoService (IFileReader fileReader = null, IVideoRepository repository = null)
         {
             _fileReader = fileReader ?? new FileReader();
+            _repository = repository ?? new IVideoRepository();
         }
         public string ReadVideoTitle()
         {
@@ -26,18 +28,11 @@ namespace TestNinja.Mocking
         {
             var viddeoIds = new List<int>();
 
-            using (var context = new VideoContext())
-            {
-                var videos =
-                    (from video in context.Videos
-                     where !video.IsProcessed
-                     select video).ToList();
+            var videos = _repository.GetUnprocessedVideos();
+            foreach (var v in videos)
+                viddeoIds.Add(v.Id);
 
-                foreach (var v in videos)
-                    videoIds.Add(v.Id);
-
-                return String.Join(",", videoIds);
-            }
+            return String.Join(",", videoIds);
         }
     }
 
