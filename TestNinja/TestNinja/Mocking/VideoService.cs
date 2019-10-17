cd6935f19cd6e16c9d9eaf4285aefcee08d432e0
Warisara33 <warisara.sa@rmuti.ac.th>
2019-10-17 16:22:22 +0700

    5_3 Refactoring Towards a Loosely-coupled Desing

---
+++ TestNinja/TestNinja/Mocking/VideoService.cs
@@ -0,0 +1,46 @@
+﻿using ...
+
+namespace TestNinja.Mocking
+{
+    public class VideoService
+    {
+        public string ReadVideoTitle()
+        {
+            var str = new FileReader().Read("video.txt");
+            var video = JsonConvert.DeserialieObject<Video>(str);
+            if (video == null)
+                return "Error parsing the video.";
+            return video.Title;
+        }
+
+        public string GetUnprocessedVideosAsCsv()
+        {
+            var viddeoIds = new List<int>();
+
+            using (var context = new VideoContext())
+            {
+                var videos =
+                    (from video in context.Videos
+                     where !video.IsProcessed
+                     select video).ToList();
+
+                foreach (var v in videos)
+                    videoIds.Add(v.Id);
+
+                return String.Join(",", videoIds);
+            }
+        }
+    }
+
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsProcessed { get; set; }
+    }
+
+    public class VideoContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+    }
+}
