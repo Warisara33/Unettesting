fd5ada45ba386dc102574f0fc520061f54bc15de
Warisara33 <warisara.sa@rmuti.ac.th>
2019-10-17 14:37:12 +0700

    4_2 Solution-FizzBuzz

--- TestNinja/TestNinja/Fundamentals/FizzBuzz.cs
+++ TestNinja/TestNinja/Fundamentals/FizzBuzz.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestNinja.Fundamentals
+﻿namespace TestNinja.Fundamentals
 {
-    class Class1
+    public class FizzBuzz
     {
+        public static string GetOutput(int number)
+        {
+            if ((number % 3 == 0) && (number % 5 == 0))
+                return "FizzBuzz";
+
+            if (number % 3 == 0)
+                return "Fizz";
+
+            if (number % 5 == 0)
+                return "Buzz";
+
+            return number.ToString();
+        }
     }
 }
